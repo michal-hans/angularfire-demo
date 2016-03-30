d90ca32b841fd9d2499111d35e7cc9d818573c11
Michal Hans <mhans@pgs-soft.com>
2016-03-30 19:55:24 +0200

    Update items

--- src/app/pages/home/home.controller.js
+++ src/app/pages/home/home.controller.js
@@ -5,8 +5,7 @@ export class HomeController {
     var ref = new Firebase("https://torid-fire-1359.firebaseio.com/apps/todo");
 
     // download the data into a local object
-    this.fireData = $firebaseArray(ref);
-    this.list = this.fireData;
+    this.list = $firebaseArray(ref);
   }
 
   addItem(newItemName) {
@@ -16,5 +15,21 @@ export class HomeController {
 
   removeItem(index) {
     this.list.$remove(index, 1);
+  }
+
+  editItem(index) {
+    this.list[index].edit = true;
+    this.list[index].original = angular.copy(this.list[index].name);
+  }
+
+  restoreItem(index) {
+    this.list[index].name = this.list[index].original;
+    delete this.list[index].edit;
+  }
+
+  updateItem(index) {
+    delete this.list[index].edit;
+    delete this.list[index].original;
+    this.list.$save(this.list[index]);
   }
 }
